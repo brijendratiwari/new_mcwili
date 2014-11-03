6a9a730f3175e65945fb172494879297e157004d
brij <brij@me.com>
2014-11-03 18:36:47 +0530

    changes.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -26,6 +26,25 @@ $(document).ready(function(){
             
         ]}
         );
+    $("#exact-target").dataTable({
+         "ordering": false,
+        "bProcessing": false,
+        "bServerSide": true,
+        "sAjaxSource": base_url + "home/get_all_et", "bDeferRender": true,
+        "aLengthMenu": [[10, 30, 50, 100, -1], [10, 30, 50, 100, $("#sAll").val()]],
+//        "sPaginationType": "full_numbers",
+        "iDisplayLength": 10,
+        "bDestroy": true, //!!!--- for remove data table warning.
+        "aoColumnDefs": [
+            {"aTargets": [0]},
+            {"sClass": " aligncenter", "aTargets": [1]},
+            {"sClass": "eamil_conform aligncenter", "aTargets": [2]},
+            {"sClass": "hidden-phone", "aTargets": [3]},
+            {"sClass": "hidden-phone", "aTargets": [4]},
+            {"sClass": "hidden-phone", "aTargets": [5]},
+            
+        ]}
+        );
     $("#black-boxx").dataTable({
          "ordering": false,
         "bProcessing": false,
